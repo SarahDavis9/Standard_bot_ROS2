6e93ed171de5a5bfbba45c6b3ebdfe91d3b2cd39
SarahDavis9 <davi8874@vandals.uidaho.edu>
2025-04-02 11:10:51 -0700

    Delete src/install directory

--- src/install/action_interfaces/share/action_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(action_interfaces_IDL_FILES "action/SetCartPos.idl;action/SetJointPos.idl;action/OnRobotGripper.idl")
-set(action_interfaces_INTERFACE_FILES "action/SetCartPos.action;action/SetJointPos.action;action/OnRobotGripper.action")
